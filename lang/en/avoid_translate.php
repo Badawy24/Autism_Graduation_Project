262c7a94ac2439d114cae7f2169cb3aee4e66ea0
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2023-04-26 03:49:05 +0200

    Avoid Page & Translate

---
+++ lang/en/avoid_translate.php
@@ -0,0 +1,15 @@
+<?php
+return
+    [
+        "guideline" => "Guideline to avoid autism",
+        "excitement_title" => "Show excitement",
+        "excitement_content" => "As a father, you must show some excitement for all of your activities, because this helps your child understand what is happening in the environment around him. For example, when you are playing a game, at the end of it, a certain shape appears. When this shape appears, look into the eyes of the child and show some surprise and joy. Also, when you notice that your son is practicing a certain activity, try to draw his attention to the fact that you are following and excited about what he is doing. This shows him that you are aware of what he is doing and are waiting for it.",
+        "follow_baby_title" => "Follow your baby's lead",
+        "follow_baby_content" => "It is natural that you know the forms of your child's games (for example, the final puzzle form), but you have to show a lot of interest and enthusiasm for everything that makes your child happy, and you must participate in that. Try to show your admiration for everything your child makes, as if you see it for the first time (for example, when your son makes a paper ship). 'For a baby who might be developing autism, you want to enter their world and make it fun,' says Dr. Dawson.",
+        "use_sing_title" => "Use Singing",
+        "use_sing_content" => "To improve communication with your child, you can associate your words with singing, for example, when you want your child to eat a meal, you can sing with him about vegetables and link singing with certain movements and gestures. 'Songs turn routine moments into chances to communicate, and because words are attached to a melody, it's easier for your baby to engage with them—especially when there are gestures that go along with what you're singing,' says Dr. Shulman.",
+        "turns_title" => "Take Turns",
+        "turns_content" => "Try to get your child to imitate you in actions, to do the thing first and then your child to do it helps your child to communicate. There are many games that help you in this, the simplest of which is the exchange of the ball (to throw the ball to him, and he throws it to you again) 'One early sign that a child has autism is that they don't initiate communication, so you want to help your baby grasp that it's something they can do,' says Dr. Shulman.",
+        "spotlight_title" => "Grab the spotlight",
+        "spotlight_content" => "Try to attract your child's attention towards you in order to make your child distinguish between people and things. You can do a dance or fun movements to draw your child's attention to you. 'A child who may have autism struggles to grasp that people are more important than objects,' says Dr. Korb",
+    ];
--- resources/views/pages/avoid.blade.php
+++ resources/views/pages/avoid.blade.php
@@ -1,50 +1,36 @@
-<?php
-@include('navbar_after_login.php');
-?>
+@extends('pages.main-template')
 
+@section('navbar')
+@include('pages.navbar_after_login')
+@endsection
+
+@section('content')
 <div class="avoid-section">
     <div class="container">
         <div class="row">
             <div class="col-md-10">
                 <div class="avoid-content">
-                    <h2>Guideline to avoid autism</h2>
+                    <h2>{{ __('avoid_translate.guideline') }}</h2>
                     <ol>
                         <div class="step">
-                            <li>Show excitement</li>
-                            <p>As a father, you must show some excitement for all of your activities, because this helps your child understand what is happening in the environment around him.
-                                <br />For example, when you are playing a game, at the end of it, a certain shape appears. When this shape appears, look into the eyes of the child and show some surprise and joy.
-                                <br />Also, when you notice that your son is practicing a certain activity, try to draw his attention to the fact that you are following and excited about what he is doing. This shows him that you are aware of what he is doing and are waiting for it.
-                            </p>
+                            <li>{{ __('avoid_translate.excitement_title') }}</li>
+                            <p>{{ __('avoid_translate.excitement_content') }}</p>
                         </div>
                         <div class="step">
-                            <li>Follow your baby's lead</li>
-                            <p>It is natural that you know the forms of your child's games (for example, the final puzzle form), but you have to show a lot of interest and enthusiasm for everything that makes your child happy, and you must participate in that.
-                                <br />
-                                Try to show your admiration for everything your child makes, as if you see it for the first time (for example, when your son makes a paper ship).
-                                <br />
-                                "For a baby who might be developing autism, you want to enter their world and make it fun," says Dr. Dawson.
-                            </p>
+                            <li>{{ __('avoid_translate.follow_baby_title') }}</li>
+                            <p>{{ __('avoid_translate.follow_baby_content') }}</p>
                         </div>
                         <div class="step">
-                            <li>Use Singing</li>
-                            <p>To improve communication with your child, you can associate your words with singing, for example, when you want your child to eat a meal, you can sing with him about vegetables and link singing with certain movements and gestures.
-                                <br />
-                                "Songs turn routine moments into chances to communicate, and because words are attached to a melody, it's easier for your baby to engage with them—especially when there are gestures that go along with what you're singing," says Dr. Shulman.
-                            </p>
+                            <li>{{ __('avoid_translate.use_sing_title') }}</li>
+                            <p>{{ __('avoid_translate.use_sing_content') }}</p>
                         </div>
                         <div class="step">
-                            <li>Take Turns</li>
-                            <p>Try to get your child to imitate you in actions, to do the thing first and then your child to do it helps your child to communicate.<br />
-                                There are many games that help you in this, the simplest of which is the exchange of the ball (to throw the ball to him, and he throws it to you again)
-                                <br />"One early sign that a child has autism is that they don't initiate communication, so you want to help your baby grasp that it's something they can do," says Dr. Shulman.
-                            </p>
+                            <li>{{ __('avoid_translate.turns_title') }}</li>
+                            <p>{{ __('avoid_translate.turns_content') }}</p>
                         </div>
                         <div class="step">
-                            <li>Grab the spotlight</li>
-                            <p>Try to attract your child's attention towards you in order to make your child distinguish between people and things. You can do a dance or fun movements to draw your child's attention to you.
-                                <br />
-                                "A child who may have autism struggles to grasp that people are more important than objects," says Dr. Korb
-                            </p>
+                            <li>{{ __('avoid_translate.spotlight_title') }}</li>
+                            <p>{{ __('avoid_translate.spotlight_content') }}</p>
                         </div>
                     </ol>
                 </div>
@@ -55,9 +41,4 @@
     </div>
 </div>
 
-
-
-
-<?php
-@include('script.php');
-?>
+@endsection
